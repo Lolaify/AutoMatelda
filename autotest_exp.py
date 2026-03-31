176a735e5f64869bb1319f7711e9ef3acd0bb510
pavel <pavel.kessler@campus.tu-berlin.de>
2026-03-31 20:03:04 +0200

    add functions

--- autotest_exp.py
+++ autotest_exp.py
@@ -1,7 +1,7 @@
 import configparser
 import os
 import pickle
-
+import numpy as np
 import pipeline
 
 def read_config(file_path):
@@ -28,6 +28,41 @@ def get_output_path(exec, config):
         + "_labels",
     )
 
+def load_experiment_result(executions: list[str], config):
+    result_dfs = {}
+    results_per_table = {}
+    for execution in executions:
+        base_path = f"{config['DIRECTORIES']['output_dir']}_{execution}"
+        result_dfs[execution] = {}
+        results_per_table[execution] = {}
+        for run in os.listdir(base_path):
+            result_df_path = os.path.join(base_path, run, "results", "results_df.pickle")
+            if os.path.exists(result_df_path):
+                with open(result_df_path, "rb") as f:
+                    result_dfs[execution][run] = pickle.load(f)
+            results_per_table_path = os.path.join(base_path, run, "results", "results_per_table.pickle")
+            if os.path.exists(results_per_table_path):
+                with open(results_per_table_path, "rb") as f:
+                    results_per_table[execution][run] = pickle.load(f)
+    return result_dfs, results_per_table
+
+def add_labels_to_result_dfs(result_dfs):
+    for execution in result_dfs.keys():
+        for run in result_dfs[execution].keys():
+            result_df = result_dfs[execution][run]
+            conditions = [
+                (result_df["predicted"] == 1) & (result_df["label"] == 1),
+                (result_df["predicted"] == 1) & (result_df["label"] == 0),
+                (result_df["predicted"] == 0) & (result_df["label"] == 0),
+                (result_df["predicted"] == 0) & (result_df["label"] == 1),
+            ]
+            choices = ["TP", "FP", "TN", "FN"]
+            result_dfs[execution][run]["result"] = np.select(conditions, choices, default="Unknown")
+
+            result_dfs[execution][run]["auto_test_overwrite"] = (result_df["auto_test_label"] == 1) & (result_df["propagated_label"] == 0)
+    return result_dfs
+
+
 def experiment(execution, config_file_path, config):
     result_df_path = os.path.join(
         get_output_path(execution, config),
@@ -49,3 +84,38 @@ config = read_config(config_file_path)
 
 experiment_output_path = get_output_path(0, config)
 """
+
+if __name__ == "__main__":
+    config_file_path = './config.ini'
+    dataset_num = 96
+    config = read_config(config_file_path)
+
+    exec = "Integration_Option_1"
+    update_config(config, "AUTO-TEST", "integration_pipeline_option", "1")
+    update_config(config, "EXPERIMENTS", "labeling_budget", f"{dataset_num * 2}")
+    experiment(exec, config_file_path, config)
+
+    exec = "Integration_Option_1"
+    update_config(config, "AUTO-TEST", "integration_pipeline_option", "1")
+    update_config(config, "EXPERIMENTS", "labeling_budget", f"{dataset_num * 6}")
+    experiment(exec, config_file_path, config)
+
+    exec = "Integration_Option_1"
+    update_config(config, "AUTO-TEST", "integration_pipeline_option", "1")
+    update_config(config, "EXPERIMENTS", "labeling_budget", f"{dataset_num * 12}")
+    experiment(exec, config_file_path, config)
+
+    exec = "Integration_Option_0"
+    update_config(config, "AUTO-TEST", "integration_pipeline_option", "0")
+    update_config(config, "EXPERIMENTS", "labeling_budget", f"{dataset_num * 2}")
+    experiment(exec, config_file_path, config)
+
+    exec = "Integration_Option_0"
+    update_config(config, "AUTO-TEST", "integration_pipeline_option", "0")
+    update_config(config, "EXPERIMENTS", "labeling_budget", f"{dataset_num * 6}")
+    experiment(exec, config_file_path, config)
+
+    exec = "Integration_Option_0"
+    update_config(config, "AUTO-TEST", "integration_pipeline_option", "0")
+    update_config(config, "EXPERIMENTS", "labeling_budget", f"{dataset_num * 12}")
+    experiment(exec, config_file_path, config)
